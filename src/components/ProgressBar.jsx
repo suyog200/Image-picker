d67d26d30061c0e8b75e219646092ae38fa24460
suyog200 <suygchari@gmail.com>
2024-03-06 16:37:34 +0530

    progress bar and setTimer for remove image model

---
+++ src/components/ProgressBar.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react';
+
+export default function ProgressBar(timer) {
+    const [remainingTime, setRemainingTime] = useState(timer);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+          setRemainingTime((prevTime) => prevTime - 10);
+        }, 10)
+    
+        return () => {
+          clearInterval(interval);
+        }
+    
+      }, [timer]);
+      
+    return (
+        <progress value={remainingTime} max={timer}/>
+    );
+}
